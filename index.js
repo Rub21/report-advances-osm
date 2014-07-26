fe573a8005a84688840dd151fe86ca076d4490f2
Rub21 <rub2106@gmail.com>
2014-07-26 13:06:13 -0500

    optimizando

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var obj_way = function() {
 		highways: {
 			v1: 0,
 			vx: 0,
-			oneway: 0,
+			oneways: 0,
 			bridges: 0
 		},
 
@@ -16,18 +16,13 @@ var obj_way = function() {
 			vx: 0
 		}
 	};
-
 };
 
 
 
 var start = argv._[0];
 var end = argv._[1];
-
-
-//obj_w1 = new obj_way();
-
-
+var users = ['Rub21', 'Luis36995', 'ediyes'];
 count = {
 	'Rub21': {
 		way: new obj_way()
@@ -48,24 +43,45 @@ for (var day = start; day <= end; day++) {
 	var handler = new osmium.Handler();
 	console.log('========================= Day:' + day.toString());
 	handler.on('way', function(way) {
-
-		if (typeof way.tags().highway !== 'undefined' && (way.user === 'Rub21' || way.user === 'Luis36995' || way.user === 'ediyes')) { //evalua las calles			
-
+		if (typeof way.tags().highway !== 'undefined' && users.indexOf(way.user) !== -1) { //evalua las calles			
 			if (way.version === 1) {
 				++count[way.user].way.highways.v1;
-
 			} else {
 				++count[way.user].way.highways.vx;
 			}
-
+			if (typeof way.tags().bridge !== 'undefined') {
+				++count[way.user].way.highways.bridges;
+			}
+			if (typeof way.tags().oneway !== 'undefined') {
+				++count[way.user].way.highways.oneways;
+			}
+		}
+		if (typeof way.tags().building !== 'undefined' && users.indexOf(way.user) !== -1) { //evalua las buildings			
+			if (way.version === 1) {
+				++count[way.user].way.buildings.v1;
+			} else {
+				++count[way.user].way.buildings.vx;
+			}
 		}
 	});
 	reader.apply(handler);
-
-
-
 }
-
-
+console.log("========== Reporte por Usuarios ===========");
 
 console.log(JSON.stringify(count));
+
+console.log("============ Reporte total ===============");
+
+total_way = new obj_way();
+
+for (var i = 0; i < users.length; i++) {
+	total_way.highways.v1 +=count[users[i]].way.highways.v1;
+	total_way.highways.vx +=count[users[i]].way.highways.vx;
+	total_way.highways.bridges +=count[users[i]].way.highways.bridges;
+	total_way.highways.oneways +=count[users[i]].way.highways.oneways;
+	total_way.buildings.v1 +=count[users[i]].way.buildings.v1;
+	total_way.buildings.vx +=count[users[i]].way.buildings.vx;
+};
+
+
+console.log(JSON.stringify(total_way));
