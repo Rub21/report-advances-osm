46f61ec4b0fff757244beed7ac09fd4a190b62c8
Rub21 <ruben@mapbox.com>
2014-10-29 22:47:58 -0500

    improve

--- others/index_forgraph.js
+++ others/index_forgraph.js
@@ -23,6 +23,7 @@ function format_num(n) {
 
 var start = argv.start;
 var end = argv.end;
+//var users = ['Rub21', 'ediyes', 'Luis36995'];
 var users = [];
 if (argv.users === '*') {
 
@@ -32,16 +33,18 @@ if (argv.users === '*') {
 		var handler = new osmium.Handler();
 		handler.on('way', function(way) {
 			if (typeof way.tags().highway !== 'undefined' && users.indexOf(way.user) == +-1) {
+
 				users.push(way.user);
+
 			}
 		});
 		reader.apply(handler);
 	}
 
+
 } else {
 	users = argv.users.split(",");
 }
-
 
 var count = {};
 for (var k = 0; k < users.length; k++) {
@@ -49,18 +52,13 @@ for (var k = 0; k < users.length; k++) {
 		way: new obj_way()
 	};
 	count[users[k]] = way;
-
 };
 
-var array_daily=[];
-
 for (var day = start; day <= end; day++) {
-
 	var file = new osmium.File(day.toString() + ".osc");
 	var reader = new osmium.Reader(file);
 	var handler = new osmium.Handler();
 	console.log('========================= Day:' + day.toString());
-
 	handler.on('way', function(way) {
 		if (typeof way.tags().highway !== 'undefined' && users.indexOf(way.user) !== -1) { //evalua las calles			
 			if (way.version === 1) {
@@ -84,11 +82,8 @@ for (var day = start; day <= end; day++) {
 		}
 	});
 	reader.apply(handler);
-
-	
-
 }
-//console.log(JSON.stringify(count));
+
 total_way = new obj_way();
 
 for (var i = 0; i < users.length; i++) {
@@ -101,46 +96,7 @@ for (var i = 0; i < users.length; i++) {
 };
 
 
+//Table Highway
 
-var array_total = [];
-var array_version1 = [];
-var array_versionx = [];
-
-for (var i = 0; i < users.length; i++) {
-
-	array_total.push(count[users[i]].way.highways.v1 + count[users[i]].way.highways.vx);
-	array_version1.push(count[users[i]].way.highways.v1);
-	array_versionx.push(count[users[i]].way.highways.vx);
-
-};
-
-
-console.log(users.toString());
-console.log(array_total.toString());
-
-
-
-//console.log(array_version1.toString());
-//console.log(array_versionx.toString());
-
-console.log('========================================================================================');
-
-
-console.log('#### Highways')
-console.log('|User | All highways | Version 1 | Version > 1 | Bridges | Oneways |');
-console.log('|---------|--------------|--------------|--------------|--------------|--------------|');
-for (var i = 0; i < users.length; i++) {
-	console.log(users[i] + '|' + format_num(count[users[i]].way.highways.v1 + count[users[i]].way.highways.vx) + '|' + format_num(count[users[i]].way.highways.v1) + '|' + format_num(count[users[i]].way.highways.vx) + '|' + format_num(count[users[i]].way.highways.bridges) + '|' + format_num(count[users[i]].way.highways.oneways));
-};
-console.log('**Total**' + '| **' + format_num(total_way.highways.v1 + total_way.highways.vx) + '** | **' + format_num(total_way.highways.v1) + '** | **' + format_num(total_way.highways.vx) + '** | **' + format_num(total_way.highways.bridges) + '** | **' + format_num(total_way.highways.oneways) + '**');
-
-
-
-//Table Buildings
-console.log('#### Buildings')
-console.log('| User | All buildings | version 1 | version > 1 |')
-console.log('|---------|--------------|--------------|--------------|')
-for (var i = 0; i < users.length; i++) {
-	console.log(users[i] + '|' + format_num(count[users[i]].way.buildings.v1 + count[users[i]].way.buildings.vx) + '|' + format_num(count[users[i]].way.buildings.v1) + '|' + format_num(count[users[i]].way.buildings.vx));
-};
-console.log('**Total**' + '| **' + format_num(total_way.buildings.v1 + total_way.buildings.vx) + '** | **' + format_num(total_way.buildings.v1) + '** | **' + format_num(total_way.buildings.vx) + '**');
+console.log('date,Rub21,ediyes,Luis36995,RichRico,dannykath')
+console.log(12, +',' + format_num(total_way.highways.v1 + total_way.highways.vx) + ',' + format_num(total_way.highways.v1) + ',' + format_num(total_way.highways.vx) + ',' + format_num(total_way.highways.bridges) + ',' + format_num(total_way.highways.oneways));
